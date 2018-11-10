049c27af3308c9d3b544f2205b16497fe2bfb3db
LightningDragonXD <jacquesdu59@gmail.com>
2018-11-10 22:20:24 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,21 @@
+var Discord = require('discord.io');
+
+var auth = require('./auth.json');
+
+var bot = new Discord.Client({
+    token: auth.token,
+    autorun: true
+});
+
+bot.on('ready', function(event){
+    console.log('Logged in as %s - %s\n', bot.username, bot.id);
+});
+
+bot.on('message', function(user, userID, channelID, message, event){
+    if(message === "ping"){
+        bot.sendMessage({
+            to: channelID,
+            message: "pong"
+        });
+    }
+});
