5d554e188e88c58bf56cca84ab3f20603fa3fce5
LightningDragonXD <jacquesdu59@gmail.com>
2018-11-10 23:05:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,16 @@
 var Discord = require('discord.io');
 var bot = new Discord.Client({
-    autorun: true,
-    token: process.env.TOKEN
+    token: process.env.TOKEN,
+    autorun: true
+    
 });
 
-bot.on('ready', function(event){
-    console.log('Logged in as %s - %s\n', bot.username, bot.id);
-    bot.connect();
+bot.on("ready", function(event){
+    console.log("Logged in as %s - %s\n", bot.username, bot.id);
+
 });
 
-bot.on('message', function(user, userID, channelID, message, event){
+bot.on("message", function(user, userID, channelID, message, event){
     if(message === "ping"){
         bot.sendMessage({
             to: channelID,
