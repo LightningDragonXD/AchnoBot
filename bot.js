f4acae37afabc9fa328afd855a8fe1e6e4965fbd
LightningDragonXD <jacquesdu59@gmail.com>
2018-11-11 18:34:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,11 +54,11 @@ function helpCommandes(message){
 				.setTimestamp()
 				.setColor("#0155FE")
 				message.channel.sendEmbed(help);
-			    }			
+			    	}else {
+        				sendError(message, "Commande inconnue.");	
+    				}					
 		    }
 	    }
-    }else {
-        sendError(message, "Commande inconnue.");	
     }
 }
 
