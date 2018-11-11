1730dcef67ebb4371cd192d715e6b3fd8e313ffe
LightningDragonXD <jacquesdu59@gmail.com>
2018-11-11 13:40:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,38 @@ bot.on('ready', function(){
 	bot.user.setActivity(prefix+'help', {type: 'WATCHING'}).catch(console.error);
 });
 
+//Fonctions des commandes du bot
+
+function sendError(message, description){
+	var erreur = new Discord.RichEmbed()
+	.setColor("#FE0000")
+	.setDiscription(':x:'+description)
+	.setTimestamp()
+	message.channel.sendEmbed(erreur);
+}
+
+function helpCommandes(message){
+	if(message.content[0] === prefix){
+		let splitHelp = message.content.split(" ");
+		if(splitHelp[0] === (prefix+"help")){
+			if(splitHelp.length === 1){
+				var help = new Discord.RichEmbed()
+				.setTitle('Liste des Commandes')
+				.setAuthor('AchnoBot',"https://i.imgur.com/pjV580Z.jpg")
+				.setDescription('Pour afficher l\'aide d\'une commande particulière ajouter en plus le nom de la commande')
+				.addField('Prefix', prefix)
+				.setTimestamp()
+				.setColor("#0155FE")
+				message.channel.sendEmbed(help);
+			}else{
+				sendError(message, "Commande inconnue.");	
+			}
+		}else{
+			sendError(message, "Commande inconnue.");	
+		}
+	}
+}
+
 bot.on('message', message => {
 	/*if(message.content === prefix+"ping"){
 		message.reply("pong");
@@ -35,33 +67,3 @@ bot.on('message', message => {
 
 bot.login(process.env.TOKEN);
 
-//Fonctions des commandes du bot
-
-function sendError(message, description){
-	var erreur = new Discord.RichEmbed()
-	.setColor("#FE0000")
-	.setDiscription(':x:'+description)
-	.setTimestamp()
-	message.channel.sendEmbed(erreur);
-}
-function helpCommandes(message){
-	if(message.content[0] === prefix){
-		let splitHelp = message.content.split(" ");
-		if(splitHelp[0] === (prefix+"help")){
-			if(splitHelp.length === 1){
-				var help = new Discord.RichEmbed()
-				.setTitle('Liste des Commandes')
-				.setAuthor('AchnoBot',"https://i.imgur.com/pjV580Z.jpg")
-				.setDescription('Pour afficher l\'aide d\'une commande particulière ajouter en plus le nom de la commande')
-				.addField('Prefix', prefix)
-				.setTimestamp()
-				.setColor("#0155FE")
-				message.channel.sendEmbed(help);
-			}else{
-				sendError(message, "Commande inconnue.");	
-			}
-		}else{
-			sendError(message, "Commande inconnue.");	
-		}
-	}
-}
