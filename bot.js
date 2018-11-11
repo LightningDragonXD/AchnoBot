1fa38958b2e665e177917409ff91c598622d6011
LightningDragonXD <jacquesdu59@gmail.com>
2018-11-11 14:37:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ function sendError(message, description){
    var embed = new Discord.RichEmbed()
             .setColor("#FE0000")
             .setDescription(':x: '+ description)
-            .setFooter('Créer par '+bot.username, 'https://i.imgur.com/pjV580Z.jpg')     
+            .setFooter('Créer par AchnoBot', 'https://i.imgur.com/pjV580Z.jpg')     
             .setTimestamp()
   message.channel.sendEmbed(embed);
 }
@@ -37,31 +37,41 @@ function helpCommandes(message){
 	}
 }
 
+function funCommandes(message){
+	if(message.content[0] === prefix){
+		let splitfun = message.content.split(" ");
+		if(splitfun[0] === prefix+"hug"){
+			if(splitfun.length === 2){
+				if(message.guild.member(message.mentions.users.first())){
+		   			var hug = new Discord.RichEmbed()
+					.setAuthor('AchnoBot', "https://i.imgur.com/pjV580Z.jpg")
+					.setDiscription(message.author.toString()+" fait un gros câlin à "+message.guild.member(message.mentions.users.first())+" ! :flushed:")
+					.setImage("https://i.imgur.com/rYrTbDQ.gif")
+					.setFooter('Créer par AchnoBot')
+					.setTimestamp()
+					.setColor("#FE9901")			
+					message.channel.sendEmbed(hug);
+				}
+			}else if(splitfun.length === 1){
+				 var hug = new Discord.RichEmbed()
+					.setAuthor('AchnoBot', "https://i.imgur.com/pjV580Z.jpg")
+					.setDiscription("AchnoBot fait un gros câlin ! :flushed:")
+					.setImage("https://i.imgur.com/rYrTbDQ.gif")
+					.setFooter('Créer par AchnoBot')
+					.setTimestamp()
+					.setColor("#FE9901")			
+					message.channel.sendEmbed(hug);
+			}else{
+				sendError(message, "Commande inconnu");	
+			}
+		}
+	}
+}
+
 bot.on('message', message => {
-	/*if(message.content === prefix+"ping"){
-		message.reply("pong");
-	}*/
+
 	helpCommandes(message);
 	
-	/*if(message.content[0] === prefix){
-		let splitHelp = message.content.split(" ");
-		if(splitHelp[0] === (prefix+"help")){
-			if(splitHelp.length === 1){
-				var help = new Discord.RichEmbed()
-				.setTitle('Liste des Commandes')
-				.setAuthor('AchnoBot',"https://i.imgur.com/pjV580Z.jpg")
-				.setDescription('Pour afficher l\'aide d\'une commande particulière ajouter en plus le nom de la commande')
-				.addField('Prefix', prefix)
-				.setTimestamp()
-				.setColor("#0155FE")
-				message.channel.sendEmbed(help);
-			}else{
-				sendError(message, "Commande inconnue.");	
-			}
-		}else{
-			sendError(message, "Commande inconnue.");	
-		}
-	}*/
 });
 
 bot.login(process.env.TOKEN);
