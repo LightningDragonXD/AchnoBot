4fd253293d71281d3cf31b6cf9baf599af1993d5
LightningDragonXD <jacquesdu59@gmail.com>
2018-11-12 14:46:45 +0100

    Delete sendError.js

--- sendError.js
+++
@@ -1,9 +0,0 @@
-var Discord = require('discord.js');
-function sendError(message, description){
-   var embed = new Discord.RichEmbed()
-            .setColor("#FE0000")
-            .setDescription(':x: '+ description)
-            .setFooter('Créer par AchnoBot')     
-            .setTimestamp()
-  message.channel.sendEmbed(embed);
-}
