b7cdd8d533a44a7ef8b53471a7d1d3f3f915f068
LightningDragonXD <jacquesdu59@gmail.com>
2018-11-12 14:36:22 +0100

    Create sendError.js

---
+++ sendError.js
@@ -0,0 +1,8 @@
+function sendError(message, description){
+   var embed = new Discord.RichEmbed()
+            .setColor("#FE0000")
+            .setDescription(':x: '+ description)
+            .setFooter('Créer par AchnoBot')     
+            .setTimestamp()
+  message.channel.sendEmbed(embed);
+}
